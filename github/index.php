74f3b3cf92bd44d92709a01ca8571b5c53aad36b
Hyouhyan <hyouhyan@hyouhyan.com>
2024-10-19 23:26:45 +0900

    feat: phpのリダイレクトをjsに変更

--- github/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-http_response_code(302);
-header("Location: https://github.com/hyouhyan");
-exit;
--- link/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-http_response_code(302);
-header("Location: https://bento.me/hyouhyan/");
-exit;
