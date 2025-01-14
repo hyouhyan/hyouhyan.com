30fc076ef4e6ac94b0c529e50d50041b5498daaa
Hyouhyan <hyouhyan@hyouhyan.com>
2025-01-15 00:31:32 +0900

    feat: Twitterリダイレクトを4でも行うように、及び使ってない関数の削除

--- js/meishi_redirect.js
+++ js/meishi_redirect.js
@@ -2,16 +2,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const meishiParam = urlParams.get('meishi');
 
-// 今日の日付を取得
-const today = new Date();
-const targetDate = new Date('2024-10-20');
-
-// パラメーターが「meishi=2」で、今日が2024年10月20日である場合、リダイレクト
-if (meishiParam === '2' && today.toDateString() === targetDate.toDateString()) {
-    window.location.href = "https://x.com/hyouhyan";
-}
-
-// パラメーターが「meishi=3」のとき、Twitterにリダイレクト
-if (meishiParam === '3') {
+// Twitterリダイレクト
+if (meishiParam == '3' || meishiParam == '4') {
     window.location.href = "https://x.com/hyouhyan";
 }
