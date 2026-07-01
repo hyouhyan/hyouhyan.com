90777ce6b0fcc39d07c38b2bb374e1e762527e64
hyouhyan <hyouhyan@hyouhyan.com>
2026-07-01 06:46:56 +0000

    feat: careerにmonthとurlを追加

--- src/data/career.ts
+++ src/data/career.ts
@@ -3,26 +3,38 @@ export interface CareerItem {
   month?: string;
   title: string;
   description: string;
+  url?: string;
 }
 
 export const career: CareerItem[] = [
   {
-    year: '2022 4月',
+    year: '2021',
+    title: '自宅サーバの運用開始',
+    description: '',
+  },
+  {
+    year: '2022',
+    month: '4月',
     title: '愛知工業大学 入学',
     description: '情報科学部 コンピュータシステム専攻',
   },
   {
-    year: '2024 5月',
+    year: '2024',
+    month: '5月',
     title: 'モバイルコンピューティング研究室(内藤研究室) 配属',
     description: 'NAPT越えを実現してセキュアなP2P通信を提供する、オーバーレイネットワークの研究を行う。',
+    url: 'https://pluslab.org',
   },
   {
-    year: '2025 5月',
+    year: '2025',
+    month: '5月',
     title: '姫雛ガジェット研究所 所属 / JOJIハウスにて活動開始',
     description: 'JOJIハウスCTOに就任。サーバ・ネットワーク管理等、主に情シスとして活動。',
+    url: 'https://jojilab.hyouhyan.com',
   },
   {
-    year: '2026 4月',
+    year: '2026',
+    month: '4月',
     title: '愛知工業大学 大学院 入学',
     description: '経営情報科学研究科',
   },
