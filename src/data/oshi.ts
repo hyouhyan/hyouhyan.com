c233feca6018dcbb9ae87eb94969d0a867bb8fa4
hyouhyan <hyouhyan@hyouhyan.com>
2026-07-03 06:54:43 +0000

    feat: 推し更新

--- src/data/oshi.ts
+++ src/data/oshi.ts
@@ -16,16 +16,31 @@ export const oshi: OshiItem[] = [
   {
     name: 'ヒメヒナ',
     category: 'VTuber',
-    img: 'https://himehina.jp/resource/1079/71a02931-1c64-4674-ac76-a3c47817b48b.png?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6XC9cL2hpbWVoaW5hLmpwXC9yZXNvdXJjZVwvMTA3OVwvNzFhMDI5MzEtMWM2NC00Njc0LWFjNzYtYTNjNDc4MTdiNDhiLnBuZyIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTc4MzE0NjUxMn19fV19&Signature=dytGpcoEgqtvs0UQJhyJVFGGA9uoKCVbjeIQDS06QhiqX8xRnhFdgLvs-4j3nSN~UXYZE433jvmTgA2BNtZGk7OIiHKS7fToWHQAt7ivqhXy3kjtvwcrAqA5vXGOANz1OBOa2T3jBEmgT3sz~iiT-6vNqj8SM4jtgbx~5wlXJ5BwjeNyQHRy4ktxVMOVnqKyU1tTIKv9bgKKOVIaaI5tydd147w7JZ4rj2xZ~tPNhpr7PwrMPOoNewADQ1pd7s5yTymAkIX1Dqib~J0UOyQRjLoPdCQK8vQCoyzGigwUwhvBzis0FRzANyVnTHlEflQOnrO-3~ce7vpU9XhcU6q9bw__&Key-Pair-Id=K1WUY38TKRJ192'
+    url: 'https://www.youtube.com/@HimeHina',
+  },
+  {
+    name: '花譜',
+    category: 'VTuber',
+    url: 'https://www.youtube.com/@virtual_kaf',
+  },
+  {
+    name: '明透',
+    category: 'VTuber',
+    url: 'https://www.youtube.com/@ASU_virtual',
+  },
+  {
+    name: 'V.W.P',
+    category: 'VTuber',
+    url: 'https://www.youtube.com/channel/UCfiSo8tO3WPU-8YOgr4Ba6g',
   },
   {
     name: 'ミライアカリ',
     category: 'VTuber',
-    img: 'https://pbs.twimg.com/profile_images/1509789168167055363/Sz-6PgAd_400x400.jpg'
+    url: 'https://www.youtube.com/channel/UCMYtONm441rBogWK_xPH9HA',
   },
   {
     name: '大蔦エル',
     category: 'VTuber',
-    img: 'https://pbs.twimg.com/profile_images/1576148710311940096/6p_BW5nw_400x400.jpg',
+    url: 'https://x.com/otsuta_l',
   },
 ];
